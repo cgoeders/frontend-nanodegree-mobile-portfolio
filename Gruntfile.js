360ab77bd0437e0a68b4279279c1a2e5c9a13712
Christina Goeders <xgoeders@gmail.com>
2015-09-23 23:48:54 -0700

    Added htmlmin (commented out copy).

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,15 +2,15 @@ module.exports = function(grunt) {
 	require('load-grunt-tasks')(grunt);
 
 	grunt.initConfig({
-		copy: {
-			dist: {
-				files: [
-					{
-						expand: true, cwd: 'src', src: ['**'], dest: 'dist/'
-					}
-				]
-			}
-		},
+		// copy: {
+		// 	dist: {
+		// 		files: [
+		// 			{
+		// 				expand: true, cwd: 'src', src: ['**'], dest: 'dist/'
+		// 			}
+		// 		]
+		// 	}
+		// },
 
 		open: {
 			dev: {
@@ -24,13 +24,46 @@ module.exports = function(grunt) {
 			},
 
 			src: ['**']
-		}
+		},
+
+		'htmlmin': {
+    		dist: { 
+      			options: {
+		        	removeComments: true,
+		        	collapseWhitespace: true
+	    		},
+	    		
+	    		//dictionary of files
+	    		files: {
+	        		//'destination': 'source'
+	        		'dist/index.min.html': 'src/index.html',
+	        		'dist/project-2048.min.html': 'src/project-2048.html',
+	        		'dist/project-mobile.min.html': 'src/project-mobile.html',
+	        		'dist/webperf.min.html': 'src/project-webperf.html',
+	        		'dist/views/pizza.min.html': 'src/views/pizza.html'
+	    		}
+	    	}
+		},
+
+		// 'cssmin': {
+
+
+		// }
+
+
+
+
+
 	});
 
 
 	grunt.registerTask('default', [
-		'copy', 'open'
+		//'copy', 
+		'htmlmin',
+		// 'cssmin',
+		'open'
+
 	]);
 
-	grunt.registerTask('upload', [ 'default', 'gh-pages'])
+	grunt.registerTask('upload', ['default', 'gh-pages'])
 }
